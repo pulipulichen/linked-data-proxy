2c227374cfdf8199ebaa0c5612bc46a267d77895
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-02-03 08:59:48 +0800

    0203 好吧，我改了啥我也想不起來了，先這樣吧

--- proxy_module/test.js
+++ proxy_module/test.js
@@ -2,6 +2,7 @@ proxy = function (_res, _query) {
     
 var _options = {
     url: "https://pulipulichen.github.io/blogger/posts/2017/01/wikipedia.html?q=" + _query,
+    // https://zh.wikipedia.org/w/index.php?title=%E6%95%B8%E4%BD%8D%E5%9C%96%E6%9B%B8%E9%A4%A8&oldformat=true&printable=yes
     encoding: "utf8",
     /*
     process: function (_content) {
@@ -13,7 +14,9 @@ var _options = {
     //select_text: "#mw-content-text > p:first",
 };
 
-    web_crawler(_res, _options);
+web_crawler(_res, _options);
+
+//_res.send(_query);
+// ----------------------------------------    
     
-    //_res.send(_query);
 };  // proxy = function (_res, _query) {
