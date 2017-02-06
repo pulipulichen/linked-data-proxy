bd9aa2b71bf6ac7de29e959f5be5eb6e98d0b9cc
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-02-06 20:38:20 +0800

    0206 git整理到一半

--- git_init.sh
+++ git_init.sh
@@ -1,6 +1,7 @@
 if [ -z "$GIT_BRANCH" ]; then GIT_BRANCH=origin/master; fi
 if [ -z "$GIT_PATH" ]; then GIT_PATH=/root/linked-data-proxy; fi
 
-cd $GIT_PATH
+cd /root/
 git clone https://github.com/pulipulichen/linked-data-proxy.git
+cd $GIT_PATH
 git reset --hard origin/master
--- git_update.sh
+++ git_update.sh
@@ -7,6 +7,6 @@ cd $GIT_PATH
 git --git-dir="$GIT_PATH"/.git clean -f -d
 git --git-dir="$GIT_PATH"/.git reset --hard "$GIT_BRANCH"
 #git --git-dir="$GIT_PATH"/.git pull --rebase --force master
-git --git-dir="$GIT_PATH"/.git pull --force kals
+git --git-dir="$GIT_PATH"/.git pull --force master
 
 git --git-dir="$GIT_PATH"/.git merge "$GIT_BRANCH" --no-commit
