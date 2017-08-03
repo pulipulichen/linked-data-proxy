dd01d69d0a6487b1bf7f4d230dc3a1b65c0d13fe
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-08-03 10:12:17 +0800

    0803 加入新的轉址

--- app/redirect.js
+++ app/redirect.js
@@ -4,5 +4,6 @@ app.get('/directory_ming', function (_req, _res) {
 
 app.get('/phppgadmin', function (_req, _res) {
     //_res.redirect('http://exp-linked-data-proxy-2017.dlll.nccu.edu.tw:3259/phppgadmin');
+    _res.setHeader('content-type', 'text/html');
     _res.send('<script type="text/javascript"> location.href="http://exp-linked-data-proxy-2017.dlll.nccu.edu.tw:3259/phppgadmin"; </script>');
 });
