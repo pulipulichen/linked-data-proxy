dc986dd2ff93bc09ac16dccacf638fc4eebe99de
wyfan <moonunique@gmail.com>
2018-12-26 19:32:34 +0800

    0729

--- proxy_module/dict.tw/dict.tw.js
+++ proxy_module/dict.tw/dict.tw.js
@@ -49,7 +49,7 @@ var _options = {
         try {
            // var _d = $(_content).find(".db_info").find(".def");
             var _second = _content.lastIndexOf("<hr>");
-            var _content_hearder = _content.lastIndexOf("<hr>", _second-1);//'</form>';
+            var _content_hearder = _content.lastIndexOf("<hr>", _second);//'</form>';
             console.log(_content_hearder);
             var _content_footer = '<hr>';
             
