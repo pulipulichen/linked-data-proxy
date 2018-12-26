830967d157a7a522656a7bd3bf0b24b35ed2f56a
wyfan <moonunique@gmail.com>
2018-12-26 13:45:20 +0800

    Update dict.tw.js

--- proxy_module/dict.tw/dict.tw.js
+++ proxy_module/dict.tw/dict.tw.js
@@ -52,7 +52,7 @@ var _options = {
         var _syllable =_s[0].text;
         
         var _d = $(_content).find(".db_info").next(".def").find(".syllable");
-        var _def = _d[0].text;
+        var _def = _d[0].value;
         
         var _removePhone = $(_d[0]).find("a").remove();
         var _removeSyllable = $(_removePhone).find("span").remove();
