4b85930a3e9d513af9f3664d1e3fbada2ced80df
wyfan <moonunique@gmail.com>
2018-12-26 12:39:52 +0800

    Update dict.tw.js

--- proxy_module/dict.tw/dict.tw.js
+++ proxy_module/dict.tw/dict.tw.js
@@ -50,7 +50,7 @@ var _options = {
         
         var _s = $(_content).find(".syllable:first");
         var _syllable =_s[0].text;
-        var _d = $(_content).find(".db_info").next(".def:first").val(); 
+        var _d = $(_content).find(".db_info").next(".def:first").value; 
         //var _def = _d[0].text();
         _content =_syllable + "  def:"+ _d;
         return _content;
